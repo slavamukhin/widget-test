f42a71ec4d71607491cf262240010d4299207601
smuhin <via4eslav.mukhin@yandex.ru>
2025-12-02 09:15:21 +0300

    build fixed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { defineCustomElement } from 'vue'
 import WeatherWidget from './WeatherWidget.vue'
-import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/themes/md-light-indigo/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 
