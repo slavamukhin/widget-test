0aa8962b298798671584a8bfe9a7eeb11bc04631
smuhin <via4eslav.mukhin@yandex.ru>
2025-12-02 22:29:00 +0300

    bundle fixed

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: 'weather-widget',
       formats: ['iife']
     },
-    minify: 'terser',
+    minify: 'esbuild',
     target: 'es2017',
     sourcemap: false,
     cssCodeSplit: false,
