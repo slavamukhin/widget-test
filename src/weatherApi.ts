03a2c8d7aa3567bc11adbbff821b606c2646d398
smuhin <via4eslav.mukhin@yandex.ru>
2025-12-02 14:21:45 +0300

    fixed url

--- src/weatherApi.ts
+++ src/weatherApi.ts
@@ -2,7 +2,7 @@ const API_KEY = "02824533832ec37dab2f769bc33aaa21";
 
 export async function fetchWeatherByCoords(lat: number, lon: number) {
   const url =
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=ru&appid=${API_KEY}`;
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
 
   const response = await fetch(url);
   if (!response.ok) throw new Error("Ошибка запроса погоды по координатам");
