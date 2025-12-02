e63dc28ae9cc3cd30b416dbe42937dc41943fee4
smuhin <via4eslav.mukhin@yandex.ru>
2025-12-02 10:17:41 +0300

    build fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue({ customElement: true })],
+  define: {
+      'process.env.NODE_ENV': '"production"'
+  },
   build: {
     lib: {
       entry: 'src/main.ts',
       name: 'WeatherWidget',
       fileName: 'weather-widget',
-      formats: ['iife'] // чтобы подключать через <script>
+      formats: ['iife']
+    },
+    rollupOptions: {
+      external: [],
     }
   }
 })
